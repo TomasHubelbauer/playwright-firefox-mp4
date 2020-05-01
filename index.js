29c696aff376a8dcb9a8a65678a69f3bc02eee4c
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-05-01 23:02:12 +0200

    Try navigating to a video page instead of a video file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const playwright = require('playwright');
 const path = require('path');
 
+process.on('uncaughtException', error => { throw error; });
+process.on('unhandledRejection', error => { throw error; });
+
 void async function () {
   const browser = await playwright.firefox.launch();
   const page = await browser.newPage();
-  await page.goto(path.join(__dirname, 'demo.mp4'));
+  await page.goto(path.join(__dirname, 'demo.html'));
+  await page.waitForFunction(() => document.querySelector('video').readyState === 4);
   await page.screenshot({ path: path.join(__dirname, 'screenshot.png') });
   await browser.close();
 }()
