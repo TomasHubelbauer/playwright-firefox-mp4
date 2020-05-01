2e82414f8bb82dd5c365ecde8745acfc0e3e6a32
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-05-01 23:05:44 +0200

    Use file protocol with video page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ process.on('unhandledRejection', error => { throw error; });
 void async function () {
   const browser = await playwright.firefox.launch();
   const page = await browser.newPage();
-  await page.goto(path.join(__dirname, 'demo.html'));
+  await page.goto('file://' + path.join(__dirname, 'demo.html'));
   await page.waitForFunction(() => document.querySelector('video').readyState === 4);
   await page.screenshot({ path: path.join(__dirname, 'screenshot.png') });
   await browser.close();
